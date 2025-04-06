be60c862b6c3cc0abcb58c7649ac3d413e6a2e31
Ashwin <ashwinfury1@gmail.com>
2025-04-05 23:43:05 -0400

    constants now has validGames array to keep track of acceptable gameTypes at runTime

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,14 @@
+import { GameType } from "../types/types";
+
 export const INITIAL_PLAYER_POINTS = 100;
 export const GAME_MAX_PLAYER_LIMIT = 6;
+
+export const validGames: GameType[] = ["blackjack", "poker"];
 
 export const ERROR_CONSTANTS = {
   LOBBY_FULL: "Lobby is full",
   LOBBY_NOT_FOUND: "Lobby not found",
   LOBBY_EXISTS: "You are already part of a lobby",
+  MISSING_PAYLOAD: "Missing Payload in socket request",
+  INVALID_GAME_TYPE: "Invalid game type",
 };
