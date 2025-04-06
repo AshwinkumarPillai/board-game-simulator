f82f5625f8f426a74d343089000eed8b339b1f4e
Ashwin <ashwinfury1@gmail.com>
2025-04-05 23:41:54 -0400

    Lobby will have its own class and all class related logic will stay here

--- src/handlers/lobbyHandler.ts
+++ src/handlers/lobbyHandler.ts
@@ -1,162 +1,107 @@
 import { Server, Socket } from "socket.io";
-import { Lobby } from "../utils/types";
-import { createInitialGameState } from "../utils/game.utils";
-import { ERROR_CONSTANTS, GAME_MAX_PLAYER_LIMIT } from "../utils/constants";
-import { createLobbyPayload, emitError, joinLobbyPayload, leaveLobbyPayload } from "../utils/socket.utils";
-import { userSockets } from "../socket";
 
-const createLobby = (
-  socket: Socket,
-  userLobbies: Map<string, string>,
-  lobbies: Map<string, Lobby>,
-  maxPlayerLimit: number
-) => {
-  console.log("MAX PLAYER LIMIT");
+import { ERROR_CONSTANTS, GAME_MAX_PLAYER_LIMIT, validGames } from "../utils/constants";
+import {
+  createLobbyPayload,
+  emitError,
+  joinLobbyPayload,
+  leaveLobbyPayload,
+  validatePayloads,
+} from "../utils/socket.utils";
+import { Lobby } from "../types/lobby";
+import { GameType } from "../types/types";
+import { lobbiesMap, userDataMap, userLobbyMap, userSockets } from "../core/state";
 
-  console.log(maxPlayerLimit);
+const createLobby = (socket: Socket, game: GameType, maxPlayerLimit: number) => {
+  try {
+    const isValid = validatePayloads(socket, {
+      game,
+      maxPlayerLimit,
+    });
 
-  if (userLobbies.has(socket.data.userId)) {
-    emitError(socket, ERROR_CONSTANTS.LOBBY_EXISTS, { lobbyId: userLobbies.get(socket.data.userId) });
-    return;
-  }
-  const lobbyId = `lobby-${Date.now()}`; // assign a unique ID
-  const lobby: Lobby = {
-    id: lobbyId,
-    players: [
-      {
-        id: socket.data.userId,
-        status: "active",
-      },
-    ],
-    owner: socket.data.userId,
-    gameState: createInitialGameState(socket.data.userId),
-    maxPlayerLimit: Math.min(maxPlayerLimit, GAME_MAX_PLAYER_LIMIT),
-    spectators: [],
-  };
-  userLobbies.set(socket.data.userId, lobbyId);
-  lobbies.set(lobbyId, lobby);
+    if (!isValid) return;
 
-  // Join the socket to the room (lobby)
-  socket.join(lobbyId);
-
-  socket.emit("lobbyCreated", lobby); // Notify the owner about lobby creation
-  console.log(`Lobby created: ${lobbyId} by ${socket.data.userId}`);
-};
-
-const joinLobby = (socket: Socket, lobbies: Map<string, Lobby>, lobbyId: string) => {
-  console.log("LobbyID: ", lobbyId);
-
-  for (const [key, value] of lobbies) {
-    console.log(`${key}: ${value}`);
-  }
-
-  const lobby = lobbies.get(lobbyId);
-  if (!lobby) {
-    emitError(socket, ERROR_CONSTANTS.LOBBY_NOT_FOUND);
-    return;
-  }
-
-  joinLobbyHelper(socket, lobby);
-
-  // Join the socket to the room (lobby)
-  socket.join(lobbyId);
-
-  socket.emit("lobbyJoined", { lobbyId, updatedLobby: lobby }); // Notify the new player
-
-  socket.to(lobbyId).emit("lobbyUpdate", lobby); // Broadcast to players in this lobby
-};
-
-const joinLobbyHelper = (socket: Socket, lobby: Lobby) => {
-  if (lobby.gameState.phase !== "lobby") {
-    if (lobby.players.find((player) => player.id === socket.data.userId)) {
-      lobby.players = lobby.players.map((player) => {
-        if (player.id === socket.data.userId) return { ...player, status: "active" };
-        return player;
-      });
-    } else {
-      lobby.spectators.push({
-        id: socket.data.userId,
-        username: socket.data.username,
-      });
-    }
-  } else {
-    if (lobby.players.length >= lobby.maxPlayerLimit) {
-      emitError(socket, ERROR_CONSTANTS.LOBBY_FULL);
+    if (!validGames.includes(game)) {
+      emitError(socket, ERROR_CONSTANTS.INVALID_GAME_TYPE, { "valid games": validGames });
       return;
     }
 
-    lobby.players.push({ id: socket.data.userId, status: "active" });
-  }
-};
-
-export const leaveLobby = (
-  socket: Socket,
-  userLobbies: Map<string, string>,
-  lobbies: Map<string, Lobby>,
-  lobbyId: string
-) => {
-  const lobby = lobbies.get(lobbyId);
-  if (!lobby) {
-    emitError(socket, ERROR_CONSTANTS.LOBBY_NOT_FOUND);
-    return;
-  }
-
-  leaveLobbyHelper(socket, userLobbies, lobby);
-  console.log(`User ${socket.data.userId} left lobby ${lobbyId}`);
-
-  // if player is the owner -> then remove all players from the lobby, notify them
-  if (lobby.owner === socket.data.userId) {
-    lobby.players.forEach((player) => {
-      userLobbies.delete(player.id);
-      const playerSocket = userSockets.get(player.id);
-      playerSocket?.emit("lobbyLeft", { lobbyId });
-      playerSocket?.leave(lobbyId);
-    });
-    lobby.players = [];
-  }
-
-  // If all players were removed (by owner leaving or the last person leaving) -> remove the lobby from the server
-  if (lobby.players.length === 0) {
-    lobbies.delete(lobbyId);
-    console.log(`Lobby ${lobbyId} deleted (no players remaining)`);
-  }
-
-  socket.emit("lobbyLeft", { lobbyId }); // Notify the user they left the lobby
-  socket.leave(lobbyId); // Leave the socket from the room (lobby)
-  socket.to(lobbyId).emit("lobbyUpdate", lobby); // Notify other players in the lobby about the update
-};
-
-const leaveLobbyHelper = (socket: Socket, userLobbies: Map<string, string>, lobby: Lobby) => {
-  if (lobby.gameState.phase !== "lobby") {
-    if (lobby.players.find((player) => player.id === socket.data.userId)) {
-      lobby.players = lobby.players.map((player) => {
-        if (player.id === socket.data.userId) return { ...player, status: "inactive" };
-        return player;
-      });
-    } else {
-      lobby.spectators = lobby.spectators.filter((spectator) => spectator.id !== socket.data.userId);
-      userLobbies.delete(socket.data.userId); // remove this user from the lobby too
+    if (userLobbyMap.has(socket.data.userId)) {
+      emitError(socket, ERROR_CONSTANTS.LOBBY_EXISTS, { lobbyId: userLobbyMap.get(socket.data.userId) });
+      return;
     }
-  } else {
-    lobby.players = lobby.players.filter((player) => player.id !== socket.data.userId);
+
+    // All server management (the maps we use) will be handled in the constructor
+    const lobby = new Lobby(userDataMap.get(socket.data.userId)!, game, maxPlayerLimit);
+
+    // Join the socket to the room (lobby)
+    socket.join(lobby.id);
+    socket.emit("lobbyCreated", lobby); // Notify the owner about lobby creation
+  } catch (error: Error | any) {
+    console.error(error);
+    emitError(socket, error.message);
   }
 };
 
-export const handleLobbyEvents = (
-  socket: Socket,
-  userLobbies: Map<string, string>,
-  lobbies: Map<string, Lobby>
-) => {
+const joinLobby = (socket: Socket, lobbyId: string) => {
+  try {
+    const lobby = lobbiesMap.get(lobbyId);
+    if (!lobby) {
+      emitError(socket, ERROR_CONSTANTS.LOBBY_NOT_FOUND);
+      return;
+    }
+
+    lobby.addPlayer(userDataMap.get(socket.data.userId)!);
+
+    // Join the socket to the room (lobby)
+    socket.join(lobbyId);
+    socket.emit("lobbyJoined", { lobbyId, updatedLobby: lobby }); // Notify the new player
+    socket.to(lobbyId).emit("lobbyUpdate", lobby); // Broadcast to players in this lobby
+  } catch (error: Error | any) {
+    console.error(error);
+    emitError(socket, error.message);
+  }
+};
+
+export const leaveLobby = (socket: Socket, lobbyId: string) => {
+  try {
+    const lobby = lobbiesMap.get(lobbyId);
+    if (!lobby) {
+      emitError(socket, ERROR_CONSTANTS.LOBBY_NOT_FOUND);
+      return;
+    }
+    lobby.removePlayer(socket.data.userId);
+    socket.emit("lobbyLeft", { message: `You left the lobby:  ${lobbyId} | ${lobby.gameType}`, lobbyId }); // Notify the user they left the lobby
+    socket.leave(lobbyId); // Leave the socket from the room (lobby)
+    socket.to(lobbyId).emit("lobbyUpdate", lobby); // Notify other players in the lobby about the update
+
+    // If the owner leaves the lobby before the game begins we destroy the lobby
+    if (lobby.gameStatus === "lobby" && lobby.ownerId === socket.data.userId) {
+      destroyLobby(lobbyId);
+    }
+  } catch (error: Error | any) {
+    console.error(error);
+    emitError(socket, error.message);
+  }
+};
+
+export const handleLobbyEvents = (socket: Socket) => {
   // Handle lobby creation
-  socket.on("createLobby", (data: createLobbyPayload) =>
-    createLobby(socket, userLobbies, lobbies, data.maxPlayerLimit)
-  );
+  socket.on("createLobby", (data: createLobbyPayload) => createLobby(socket, data.game, data.maxPlayerLimit));
 
   // Handle joining a lobby
-  socket.on("joinLobby", (data: joinLobbyPayload) => joinLobby(socket, lobbies, data.lobbyId));
+  socket.on("joinLobby", (data: joinLobbyPayload) => joinLobby(socket, data.lobbyId));
 
   // Handle leaving a lobby
-  socket.on("leaveLobby", (data: leaveLobbyPayload) =>
-    leaveLobby(socket, userLobbies, lobbies, data.lobbyId)
-  );
+  socket.on("leaveLobby", (data: leaveLobbyPayload) => leaveLobby(socket, data.lobbyId));
+};
+
+// Helper to destroy a lobby
+const destroyLobby = (lobbyId: string) => {
+  const lobby = lobbiesMap.get(lobbyId);
+  if (!lobby) return;
+
+  for (const player of lobby.players) {
+    leaveLobby(userSockets.get(player.id)!, lobbyId);
+  }
 };
